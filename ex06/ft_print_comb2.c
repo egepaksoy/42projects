dc569ba83fc3b160f35eac647b1edf22e3a83fb5
Ege Paksoy <egepaskoy@gmail.com>
2023-02-10 15:26:34 +0300

    ex07 githubdan duzeltildi

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,70 +6,46 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 16:52:19 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/02/10 15:19:41 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/02/10 15:26:10 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_ints(int i1, int i2, int i3, int i4)
+void	ft_putchar(char c)
 {
-	char	c1;
-	char	c2;
-	char	c3;
-	char	c4;
-
-	c1 = i1 + 48;
-	c2 = i2 + 48;
-	c3 = i3 + 48;
-	c4 = i4 + 48;
-	write(1, &c1, 1);
-	write(1, &c2, 1);
-	write(1, " ", 1);
-	write(1, &c3, 1);
-	write(1, &c4, 1);
-	if (i1 != 9 || i2 != 8 || i3 != 9 || i4 != 9)
-		write(1, ", ", 2);
+	write(1, &c, 1);
 }
 
-void	ft_loop(int i1, int i2, int i3, int i4)
+void	ft_print_2_digits_number(int a, int b)
 {
-	while (i1 < 10)
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (a != 98)
 	{
-		i2 = 0;
-		while (i2 < 10)
-		{
-			i3 = i1;
-			while (i3 < 10)
-			{
-				i4 = i2 + 1;
-				while (i4 < 10)
-				{
-					ft_print_ints(i1, i2, i3, i4);
-					i4 += 1;
-				}
-				i3++;
-			}
-			i2++;
-		}
-		i1++;
+		ft_putchar(',');
+		ft_putchar(' ');
 	}
 }
 
 void	ft_print_comb2(void)
 {
-	int	i1;
-	int	i2;
-	int	i3;
-	int	i4;
+	int	a;
+	int	b;
 
-	i1 = 0;
-	i2 = 0;
-	i3 = 0;
-	i4 = 0;
-	ft_loop(i1, i2, i3, i4);
-}
-int main() 
-{
-    ft_print_comb2();
+	a = 00;
+	b = 00;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_print_2_digits_number(a, b);
+			b++;
+		}
+		a++;
+	}
 }
