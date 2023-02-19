dc45d0b4e938a911c76c27423238a2b66aa8c303
Ege Paksoy <egepaskoy@gmail.com>
2023-02-19 05:34:42 +0300

    ok

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 03:44:04 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/02/18 05:25:22 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/02/19 05:10:51 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@ int	ft_recursive_power(int nb, int power)
 {
 	int	res;
 
-	res = 0;
-	if (power)
+	res = nb;
+	if (power > 1)
 		res = nb * ft_recursive_power(nb, power - 1);
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
 	return (res);
 }
--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,38 +6,33 @@
 /*   By: epaksoy <epaksoy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 05:08:46 by epaksoy           #+#    #+#             */
-/*   Updated: 2023/02/18 05:22:22 by epaksoy          ###   ########.fr       */
+/*   Updated: 2023/02/19 05:32:53 by epaksoy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
 	int	n;
-	int	o;
 
 	n = nb;
-	o = 1;
 	while (n > 1)
 	{
 		if (!(nb % n) && nb != n)
-			o = 0;
+			return (0);
 		n--;
 	}
-	if (nb > 1 && o)
-		return (1);
-	return (0);
+	if (nb < 2)
+		return (0);
+	return (1);
 }
 
 int	ft_find_next_prime(int nb)
 {
-	int	n;
-
-	n = nb;
-	while (n > 1)
+	while (nb > 2)
 	{
-		if (ft_is_prime(n))
-			return (n);
-		n++;
+		if (ft_is_prime(nb))
+			return (nb);
+		nb++;
 	}
 	return (2);
 }
