7be253c9be1602f047411792b02346014ed7159d
Ege Paksoy <egepaskoy@gmail.com>
2023-02-19 13:54:28 +0300

    ok

---
+++ ex03/ft_sort_params.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: epaksoy <epaksoy@student.42kocaeli.com.tr  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2023/02/18 05:41:23 by epaksoy		   #+#	#+#			 */
+/*   Updated: 2023/02/19 11:26:20 by epaksoy          ###   ########.fr       */
+/*																			*/
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print(char *c)
+{
+	int	n;
+
+	n = 0;
+	while (c[n])
+		n++;
+	write(1, c, n);
+	write(1, "\n", 1);
+}
+
+int	check(int i, int *arr, int ac)
+{
+	int	n;
+
+	n = 0;
+	while (n < ac)
+	{
+		if (arr[n] == i)
+			return (0);
+		n++;
+	}
+	return (1);
+}
+
+void	swap(char **av, int i)
+{
+	char	*max;
+	int		n;
+
+	n = 0;
+	while (av[i][n] || av[i + 1][n])
+	{
+		if (av[i][n] > av[i + 1][n])
+		{
+			max = av[i + 1];
+			av[i + 1] = av[i];
+			av[i] = max;
+			break ;
+		}
+		if (av[i][n] == av[i + 1][n])
+			n++;
+		if (av[i][n] < av[i + 1][n])
+			break ;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		n;
+	int		i;
+
+	n = 1;
+	while (n < ac - 1)
+	{
+		i = 1;
+		while (i < ac - 1)
+		{
+			swap(av, i);
+			i++;
+		}
+		n++;
+	}
+	n = 1;
+	while (n < ac)
+	{
+		ft_print(av[n]);
+		n++;
+	}
+}
